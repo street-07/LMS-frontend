27593bdb4fb507c127c9a620c76372b1044568e1
street-07 <ujjwalmehta6969@gmail.com>
2026-01-12 02:31:12 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://lms-4p2z.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
